09914c6af30f9cabcdcba9023d296f92251c303a
anamux <anaamux@gmail.com>
2024-01-15 10:40:11 -0300

    Layout created

--- app/src/main/java/br/com/ana/lemonade/MainActivity.kt
+++ app/src/main/java/br/com/ana/lemonade/MainActivity.kt
@@ -3,13 +3,27 @@ package br.com.ana.lemonade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.ana.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +33,12 @@ class MainActivity : ComponentActivity() {
             LemonadeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .wrapContentSize(Alignment.Center),
+                    color = MaterialTheme.colorScheme.background,
                 ) {
-                    Lemonade("Android")
+                    Lemonade()
                 }
             }
         }
@@ -30,17 +46,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Lemonade(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Lemonade() {
+    ImageAndText(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
     )
 }
+@Composable
+fun ImageAndText(modifier: Modifier = Modifier ) {
+    var result by remember { mutableStateOf(1) }
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { result = (1) }) {
+            Image(
+                painter = painterResource(R.drawable.lemon_tree),
+                contentDescription = stringResource(R.string.lemon_tree_content_description)
+            )
+            
+        }
 
-@Preview(showBackground = true)
+        Spacer(modifier = Modifier.height(32.dp))
+        Text(text = stringResource(R.string.tap_tree))
+
+    }
+}
+
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+    )
 @Composable
 fun GreetingPreview() {
     LemonadeTheme {
-        Lemonade("Android")
+        Lemonade()
     }
 }
